d756b4ee87e5207c02259052134ac787d1426738
Alex <a.smaglii@dirion.biz>
2022-11-15 17:01:32 +0300

    UpDate

---
+++ src/main/java/org/example/javatutorials/JavaRushMain.java
@@ -0,0 +1,14 @@
+package org.example.javatutorials;
+
+public class JavaRushMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        int sum = plus(5,4);
+        System.out.println(sum);
+    }
+
+    public static int plus(int a, int b) {
+        return a + b;
+    }
+}
