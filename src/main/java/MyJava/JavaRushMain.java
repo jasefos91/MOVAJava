89bbe6db1065dad029cd1df630920220c6d4bff9
Alex <a.smaglii@dirion.biz>
2022-12-06 11:33:21 +0300

    JavaAqua

---
+++ src/main/java/MyJava/JavaRushMain.java
@@ -0,0 +1,55 @@
+package MyJava;
+import java.util.Scanner;
+import java.sql.Statement;
+
+public class JavaRushMain {
+
+    static public void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Input Something");
+        String string = s.nextLine();
+        System.out.println("Super"+string);
+
+
+
+
+
+
+    }
+}
+
+
+
+
+/*  int myInt = 5;
+        if (myInt<10){
+            System.out.println("Go Home");
+        } else if (myInt<20){
+            System.out.println("Stay");
+        } else {
+            System.out.println("Arguments fail");
+        }
+
+
+    */
+        //int value = 1;
+       // while (value<10)
+       // {
+         ///   System.out.println("Hi"+value);
+           // value = value+1;
+      ///  }
+
+        // String s = "Merry";
+        // System.out.println("Christmas");
+        // System.out.println("Merry New");
+        // System.out.println("weekend");
+        //System.out.print("Happy New");
+        // System.out.println("weekend");
+        // System.out.println(s);
+        // System.out.print("Merry New!");
+        //System.out.print(" ");
+        //System.out.println("Year");
+
+
+
+
